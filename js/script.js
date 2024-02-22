9c723e2ae44b5a4829a09e7486d5a06a45b69d1c
SamNwaoshai <snwaoshai@student.sdccd.edu>
2024-02-21 18:39:06 -0800

    Upload to git

---
+++ js/script.js
@@ -0,0 +1,177 @@
+//STEP 1
+/* 
+let someMonth
+function theMonth
+let currentMonth
+let summerMonth
+let MyLibraryFuntion 
+*/
+
+//STEP 2
+/* 
+10 <-- numeric literal expression 
+'FYI: Hallway is flooded' <-- string literal expression
+true <-- Boolean literal expression
+null <-- null literal expression
+ */
+
+//STEP 3 
+/* 
+let warning = 'Watch Out!'
+let moodNumber = prompt('Rate how you are feeling on a scale of 1-10')
+ */
+
+//STEP 4
+/* 
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let intZipCode
+let intYourAge
+let strReferralSource
+let blnMayWeContactYou 
+*/
+
+//STEP 5
+/* 
+let strFirstName
+strFirstName = 'Samantha'
+
+let strLastName = 'Nwaoshai'
+
+let intZipCode; intZipCode = 92101
+*/
+
+//STEP 6
+/* 
+let monthsAlive = 360
+alert('Samantha has been alive for' + monthsAlive + 'months')
+*/
+
+//STEP 7
+/* 
+let isAlive = true
+alert('Is Samantha alive?' + isAlive)
+
+let monthsAlive = 30
+alert(monthsAlive + isAlive)
+*/
+
+//STEP 8
+/* 
+No, it is not valid because the double quotes within the single quotes plus the contractions would cause a problem.  See fix below.
+
+let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+console.log(someString) 
+*/
+
+//STEP 9
+/* 
+let galaxyAddress = null
+console.log(galaxyAddress)
+
+let homeAddress
+console.log(homeAddress) 
+*/
+
+//STEP 10
+/* 
+-string- 
+console.log(type of 'Samantha')
+
+-number-
+let x = 15
+console.log(x++)
+
+-Boolean-
+let x = 15
+Let y = 20
+console.log(x <= y != true)
+
+-undefined-
+let x
+console.log(x)
+*/
+
+//STEP 11
+/* 
+let x = 'Hello'
+let y = 'welcome to the JavaScript class!'
+alert(x + ' Samantha Nwaoshai' + y)
+*/
+
+//STEP 12
+/* 
+let x = 'Hello'
+let y = ',welcome to the JavaScript class!'
+let Name = 'Samantha Nwaoshai'
+alert(x +  ' ' + Name + y)
+*/
+
+//STEP 13
+/* 
+let x = 'Hello'
+let y = ',welcome to the'
+let Name = 'Samantha Nwaoshai'
+let course = 'JavaScript'
+alert(x +  ' ' + Name + y + ' ' + course + ' ' + 'class!') 
+*/
+
+//STEP 14
+/* 
+let x = 'Hello'
+let y = '\n Welcome to the'
+let Name = ' Samantha Nwaoshai.'
+let course = 'JavaScript'
+let stringValue = x + Name + y + course + 'class'
+alert(stringValue)
+*/
+
+//STEP 15
+/* 
+let x = 'Hello'
+let y = '\n Welcome to the'
+let Name = prompt('What is your name?')
+let course = 'JavaScript'
+let stringValue = x + Name + '.' + y + course + 'class'
+alert(stringValue)
+*/
+
+//STEP 16
+/* 
+let x = 'Hello'
+let y = '\n Welcome to the'
+let Name = prompt('What is your name?')
+let course = prompt('What class are you taking?')
+let stringValue = x + Name + '.' + y + course + 'class'
+alert(stringValue)
+*/
+
+//STEP 17
+/* 
+let x = 10
+let y = 20
+console.log(x + y)
+*/
+
+//STEP 18
+/* 
+let x = 20
+console.log(x += 20)
+*/
+
+//STEP 19
+/* 
+let x = 20
+console.log(x *= 5) 
+*/
+
+//STEP 20
+/* 
+let x = 2
+console.log(x /= 20)
+*/
+
+
